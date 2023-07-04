58a47bc9480f9f2931f97e60c4acfaa69d37830d
DS0708 <tlsehdtjd070@naver.com>
2023-07-04 23:07:16 +0900

    add Package /String_/src/main/java

--- String_/src/main/java/problem/ex_27866_.java
+++ String_/src/main/java/problem/ex_27866_.java
@@ -1,9 +1,11 @@
+package problem;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class ex_27866 {
+public class ex_27866_ {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
