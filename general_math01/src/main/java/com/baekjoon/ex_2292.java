0b79f7234f51cec492da726715c389a7f7bca4de
DS0708 <tlsehdtjd070@naver.com>
2023-09-12 17:30:42 +0900

    ex_2292

--- general_math01/src/main/java/com/baekjoon/ex_2292.java
+++ general_math01/src/main/java/com/baekjoon/ex_2292.java
@@ -1,5 +1,19 @@
 package com.baekjoon;
 
 public class ex_2292 {
-    
+    public static void main(String[] args) {
+
+        int n = new java.util.Scanner(System.in).nextInt();
+        int floor = 1;
+        int boundary = 1;
+
+        while(true) {
+            boundary += (floor-1)*6;
+
+            if(boundary>=n) break;
+
+            floor++;
+        }
+        System.out.println(floor);
+    }
 }
